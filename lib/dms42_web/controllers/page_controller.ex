6dc6ec078d50bcce3c0166facf11d7fc7f8d25c6
Benjamin Burgy <minidfx@gmail.com>
2020-03-25 22:27:59 +0100

    Disable the cache for the home single page application.

--- lib/dms42_web/controllers/page_controller.ex
+++ lib/dms42_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule Dms42Web.PageController do
   use Dms42Web, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> update_resp_header("cache-control", "no-cache", fn _ -> "no-cache" end)
+    |> render("index.html")
   end
 end

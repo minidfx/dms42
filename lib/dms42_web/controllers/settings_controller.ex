81b7050e99e57e9ea4f6734958b3e22c10d939ef
Benjamin Burgy <minidfx@gmail.com>
2020-04-13 16:26:35 +0200

    Set the json as response type.

--- lib/dms42_web/controllers/settings_controller.ex
+++ lib/dms42_web/controllers/settings_controller.ex
@@ -7,16 +7,20 @@ defmodule Dms42Web.SettingsController do
 
   def process_all_thumbnails(conn, _) do
     DocumentsManager.generate_thumbnails()
-    conn |> send_resp(200, "")
+    conn
+    |> send_resp(200, "")
   end
 
   def process_all_ocrs(conn, _) do
     DocumentsManager.generate_ocrs()
-    conn |> send_resp(200, "")
+    conn
+    |> send_resp(200, "")
   end
 
   def get_queue_info(conn, _) do
     queue_info = Dms42.QueueDocuments.info()
-    conn |> send_resp(200, queue_info |> Poison.encode!())
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, queue_info |> Poison.encode!())
   end
 end

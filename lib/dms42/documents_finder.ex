41dcfd68f34ff5af6b4906493a204016be2f1399
Benjamin Burgy <minidfx@gmail.com>
2020-04-19 18:15:36 +0200

    Improve the finder, avoid to query the SQL when the accumulator contains already the maximum results and try to search from OCR splitting the query in terms.

--- lib/dms42/documents_finder.ex
+++ lib/dms42/documents_finder.ex
@@ -5,7 +5,8 @@ defmodule Dms42.DocumentsFinder do
   alias Dms42.Models.Tag
   alias Dms42.Models.SearchResult
 
-  import Ecto.Query, only: [from: 2]
+  import Ecto.Query, only: [from: 2, where: 3, order_by: 3, limit: 3, select: 3]
+  import Ecto.Query.API, only: [ilike: 2, and: 2]
 
   @max_result 20
 
@@ -16,9 +17,12 @@ defmodule Dms42.DocumentsFinder do
   def find(query) when is_bitstring(query) do
     query
     |> normalize
-    |> find_by_tags
-    |> find_by_comments
-    |> find_by_ocr_and_filename
+    |> find_by_exact_tags
+    |> find_by_partial_tags
+    |> find_by_exact_comments
+    |> find_by_exact_ocr
+    |> find_by_partial_ocr
+    |> to_result
     |> Enum.sort_by(fn %SearchResult{:ranking => x} -> x end)
     |> Enum.uniq_by(fn %SearchResult{:document_id => x} -> x end)
   end
@@ -36,58 +40,42 @@ defmodule Dms42.DocumentsFinder do
 
   ##### Private members
 
-  @spec find_by_tags(String.t()) :: {String.t(), list(Dms42.Models.Document.t())}
-  defp find_by_tags(query) do
-    exact_result =
+  @spec to_result({String.t(), list}) :: list
+  defp to_result({_, result}), do: result
+
+  @spec find_by_exact_tags(String.t()) :: {String.t(), list(Dms42.Models.Document.t())}
+  defp find_by_exact_tags(query) do
+    result =
       query
       |> String.split(" ")
-      |> Enum.map(fn x -> x |> normalize |> find_by_exact_tag end)
+      |> Enum.map(fn x -> x |> normalize |> query_find_by_exact_tag end)
       |> Enum.map(&Dms42.Repo.all/1)
       |> Enum.flat_map(fn x -> x end)
       |> Enum.map(fn x -> to_search_result(x, 1) end)
 
-    partial_result =
+    {query, result}
+  end
+
+  @spec find_by_partial_tags(String.t()) :: {String.t(), list(Dms42.Models.Document.t())}
+  defp find_by_partial_tags({query, acc}) when length(acc) >= @max_result, do: {query, acc}
+
+  defp find_by_partial_tags({query, acc}) do
+    result =
       query
       |> String.split(" ")
-      |> Enum.map(fn x -> x |> normalize |> find_by_partial_tag end)
+      |> Enum.map(fn x -> x |> normalize |> query_find_by_partial_tag end)
       |> Enum.map(&Dms42.Repo.all/1)
       |> Enum.flat_map(fn x -> x end)
       |> Enum.map(fn x -> to_search_result(x, 2) end)
 
-    {query, exact_result ++ partial_result}
+    {query, acc ++ result}
   end
 
-  @spec find_by_exact_tag(String.t()) :: Ecto.Queryable.t()
-  defp find_by_exact_tag(term) do
-    from(d in Document,
-      left_join: dt in DocumentTag,
-      on: d.document_id == dt.document_id,
-      left_join: t in Tag,
-      on: t.tag_id == dt.tag_id,
-      or_where: t.name_normalized == ^term,
-      order_by: d.inserted_at,
-      limit: @max_result,
-      select: d
-    )
-  end
-
-  @spec find_by_partial_tag(String.t()) :: Ecto.Queryable.t()
-  defp find_by_partial_tag(term) do
-    from(d in Document,
-      left_join: dt in DocumentTag,
-      on: d.document_id == dt.document_id,
-      left_join: t in Tag,
-      on: t.tag_id == dt.tag_id,
-      or_where: ilike(t.name_normalized, ^"%#{term}%"),
-      order_by: d.inserted_at,
-      limit: @max_result,
-      select: d
-    )
-  end
-
-  @spec find_by_comments({String.t(), list(Dms42.Models.Document.t())}) ::
+  @spec find_by_exact_comments({String.t(), list(Dms42.Models.Document.t())}) ::
           {String.t(), list(Dms42.Models.SearchResult.t())}
-  defp find_by_comments({query, acc}) do
+  defp find_by_exact_comments({query, acc}) when length(acc) >= @max_result, do: {query, acc}
+
+  defp find_by_exact_comments({query, acc}) do
     result =
       from(d in Document,
         where: ilike(d.comments, ^"%#{query}%"),
@@ -101,14 +89,15 @@ defmodule Dms42.DocumentsFinder do
     {query, result ++ acc}
   end
 
-  @spec find_by_ocr_and_filename({String.t(), list(Dms42.Models.Document.t())}) ::
+  @spec find_by_exact_ocr({String.t(), list(Dms42.Models.Document.t())}) ::
           list(Dms42.Models.SearchResult.t())
-  defp find_by_ocr_and_filename({query, acc}) do
+  defp find_by_exact_ocr({query, acc}) when length(acc) >= @max_result, do: {query, acc}
+
+  defp find_by_exact_ocr({query, acc}) do
     result =
       from(d in Document,
         left_join: o in DocumentOcr,
         on: d.document_id == o.document_id,
-        or_where: ilike(d.original_file_name_normalized, ^"%#{query}%"),
         or_where: ilike(o.ocr_normalized, ^"%#{query}%"),
         order_by: d.inserted_at,
         limit: @max_result,
@@ -117,7 +106,86 @@ defmodule Dms42.DocumentsFinder do
       |> Dms42.Repo.all()
       |> Enum.map(fn x -> to_search_result(x, 4) end)
 
-    result ++ acc
+    {query, result ++ acc}
+  end
+
+  @spec find_by_partial_ocr({String.t(), list(Dms42.Models.Document.t())}) ::
+          list(Dms42.Models.SearchResult.t())
+  defp find_by_partial_ocr({query, acc}) when length(acc) >= @max_result, do: {query, acc}
+
+  defp find_by_partial_ocr({query, acc}) do
+    result =
+      query
+      |> query_find_by_partial_ocr
+      |> Enum.map(fn x -> to_search_result(x, 5) end)
+
+    {query, result ++ acc}
+  end
+
+  @spec query_find_by_exact_tag(String.t()) :: Ecto.Queryable.t()
+  defp query_find_by_exact_tag(term) do
+    from(d in Document,
+      left_join: dt in DocumentTag,
+      on: d.document_id == dt.document_id,
+      left_join: t in Tag,
+      on: t.tag_id == dt.tag_id,
+      or_where: t.name_normalized == ^term,
+      order_by: d.inserted_at,
+      limit: @max_result,
+      select: d
+    )
+  end
+
+  @spec query_find_by_partial_tag(String.t()) :: Ecto.Queryable.t()
+  defp query_find_by_partial_tag(term) do
+    from(d in Document,
+      left_join: dt in DocumentTag,
+      on: d.document_id == dt.document_id,
+      left_join: t in Tag,
+      on: t.tag_id == dt.tag_id,
+      or_where: ilike(t.name_normalized, ^"%#{term}%"),
+      order_by: d.inserted_at,
+      limit: @max_result,
+      select: d
+    )
+  end
+
+  @spec query_find_by_exact_ocr(String.t()) :: Ecto.Queryable.t()
+  def query_find_by_exact_ocr(term) do
+    from(d in Document,
+      left_join: o in DocumentOcr,
+      on: d.document_id == o.document_id,
+      or_where: ilike(o.ocr_normalized, ^"%#{term}%"),
+      order_by: d.inserted_at,
+      limit: @max_result,
+      select: d
+    )
+  end
+
+  @spec query_find_by_partial_ocr(String.t()) :: Ecto.Queryable.t()
+  def query_find_by_partial_ocr(query) do
+    terms = query |> normalize |> String.split(" ")
+
+    case terms do
+      [_] ->
+        []
+
+      terms ->
+        base_query =
+          from(d in Document,
+            left_join: o in DocumentOcr,
+            on: d.document_id == o.document_id
+          )
+
+        terms
+        |> Enum.reduce(base_query, fn x, acc ->
+          acc |> where([d, o], ilike(o.ocr_normalized, ^"%#{x}%"))
+        end)
+        |> order_by([d, o], d.inserted_at)
+        |> limit([d, o], @max_result)
+        |> select([d, o], d)
+        |> Dms42.Repo.all()
+    end
   end
 
   @spec to_search_result(Dms42.Models.Document.t(), integer) :: Dms42.Models.SearchResult.t()
